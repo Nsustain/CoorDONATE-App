a535771a8628a1dec12eef49852aad43ab1b1378
aklile-yilma <aklileyilma@gmail.com>
2023-05-09 15:25:58 +0300

    feat(global styles): added global colors and fonts

---
+++ lib/core/utils/constants/styles.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// setup colors, gradients, padding size, font-size
+
+// colors
+const kPrimaryColor = Color(0xFF4D97CF);
+const kSecondaryColor = Color(0xFFD2372E);
+const kWhiteColor = Color(0xFFF5F5F5);
+const kGrayColor = Color(0xFFE8E6E7);
+const kBlackColor = Color(0xFF111111);
+const kDangerColor = Color(0xFFD2372E);
+const kSuccessColor = Color(0xFF109410);
+const kWarningColor = Color(0xFFFF8100);
+
+// Background Colors
+const kBackgroundColor = Color(0xFFFFFFFF);
+
+// Text Colors
+const kTextPrimaryColor = Color(0xFF262626);
+const kTextSecondaryCoor = kGrayColor;
+
+// const values
+const double kDefaultPadding = 20.0;
