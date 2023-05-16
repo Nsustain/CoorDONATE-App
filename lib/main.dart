43474b954297fe0d12f78b9c5105647257f1da8e
aklile-yilma <aklileyilma@gmail.com>
2023-05-16 10:43:41 +0300

    fix(main.dart): remove signup import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:coordonate_app/core/utils/global/environment.dart';
-import 'package:coordonate_app/features/auth/presentation/screen/signup_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -19,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Signup(),
+      home: const Text("home"),
     );
   }
 }
