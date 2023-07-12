288ecccbeb353b05fa0c04fc97db25edd59643d0
aklile-yilma <aklileyilma@gmail.com>
2023-07-11 20:30:24 -0400

    feat(login): add blog consumption

--- lib/features/auth/presentation/screen/login_page.dart
+++ lib/features/auth/presentation/screen/login_page.dart
@@ -1,7 +1,13 @@
+import 'package:coordonate_app/dependency_injection.dart';
+import 'package:coordonate_app/features/auth/presentation/bloc/login/login_bloc.dart';
+import 'package:coordonate_app/features/auth/presentation/bloc/login/login_event.dart';
+import 'package:coordonate_app/features/auth/presentation/bloc/login/login_state.dart';
 import 'package:fancy_password_field/fancy_password_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import '../../../../utils//constants/styles.dart';
 
 class LoginPage extends StatefulWidget {
@@ -12,15 +18,10 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  //TextControllers
-  final emailController = TextEditingController();
-  final passwordController = FancyPasswordController();
-  //formkey
-  final formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => sl<RegisterBloc>(),
+      create: (context) => sl<LoginBloc>(),
       child: MaterialApp(
         title: 'Login Page',
         theme: Theme.of(context),
@@ -33,6 +34,12 @@ class _LoginPageState extends State<LoginPage> {
 }
 
 Widget buildLoginPage(BuildContext context) {
+  //TextControllers
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  //formkey
+  final _formkey = GlobalKey<FormState>();
+
   return Theme(
     data: Theme.of(context),
     child: Scaffold(
@@ -50,118 +57,154 @@ Widget buildLoginPage(BuildContext context) {
       ),
       body: SingleChildScrollView(
         physics: const AlwaysScrollableScrollPhysics(),
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image.asset('assets/images/login/group-pana.png'),
-                Form(
-                    key: _formkey,
-                    child: Column(children: [
-                      TextFormField(
-                        controller: emailController,
-                        decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.mail), labelText: 'Email'),
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: BlocBuilder<LoginBloc, LoginState>(
+                builder: (context, state) {
+                  if (state is LoginLoadingState) {
+                    return Container(
+                      color: Colors.white,
+                      child: Center(
+                        child: LoadingAnimationWidget.flickr(
+                          leftDotColor: const Color(0xFF1A1A3F),
+                          rightDotColor: kPrimaryColor,
+                          size: 5.w,
+                        ),
                       ),
-                      TextFormField(
-                        controller: passwordController,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.lock),
-                            labelText: 'Password'),
-                      ),
-                    ])),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                      onPressed: () {}, child: const Text('Forgot Password?')),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                        minimumSize: const Size.fromHeight(50),
-                        foregroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10))),
-                    child: const Text("Login")),
-                const Align(
-                  alignment: Alignment.center,
-                  child: Text('or'),
-                ),
-                Row(
+                    );
+                  } else {
+                    return Container();
+                  }
+                },
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.blue,
-                            width: 1.0,
+                    Image.asset('assets/images/login/group-pana.png'),
+                    Form(
+                        key: _formkey,
+                        child: Column(children: [
+                          TextFormField(
+                            controller: emailController,
+                            decoration: const InputDecoration(
+                                prefixIcon: Icon(Icons.mail),
+                                labelText: 'Email'),
                           ),
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(5.0))),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 16.0, vertical: 4.0),
-                        child: SvgPicture.asset(
-                          'assets/images/login/icons8-meta.svg',
-                          semanticsLabel: 'Meta svg',
-                          width: 25,
-                          height: 25,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.blue,
-                            width: 1.0,
+                          TextFormField(
+                            controller: passwordController,
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                                prefixIcon: Icon(Icons.lock),
+                                labelText: 'Password'),
                           ),
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(5.0))),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 16.0, vertical: 4.0),
-                        child: SvgPicture.asset(
-                          'assets/images/login/icons8-apple.svg',
-                          semanticsLabel: 'Apple svg',
-                          width: 25,
-                          height: 25,
-                        ),
-                      ),
+                        ])),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                          onPressed: () {},
+                          child: const Text('Forgot Password?')),
                     ),
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.blue,
-                            width: 1.0,
+                    SizedBox(
+                      height: 50,
+                    ),
+                    BlocBuilder<LoginBloc, LoginState>(
+                      builder: (context, state) {
+                        return ElevatedButton(
+                            onPressed: () {
+                              BlocProvider.of<LoginBloc>(context).add(
+                                  LoginButtonPressedEvent(
+                                      email: emailController.text,
+                                      password: passwordController.text));
+                            },
+                            style: ElevatedButton.styleFrom(
+                                minimumSize: const Size.fromHeight(50),
+                                foregroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10))),
+                            child: const Text("Login"));
+                      },
+                    ),
+                    const Align(
+                      alignment: Alignment.center,
+                      child: Text('or'),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.blue,
+                                width: 1.0,
+                              ),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(5.0))),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 16.0, vertical: 4.0),
+                            child: SvgPicture.asset(
+                              'assets/images/login/icons8-meta.svg',
+                              semanticsLabel: 'Meta svg',
+                              width: 25,
+                              height: 25,
+                            ),
                           ),
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(5.0))),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 16.0, vertical: 4.0),
-                        child: SvgPicture.asset(
-                          'assets/images/login/icons8-google.svg',
-                          width: 25,
-                          height: 25,
-                          semanticsLabel: 'Google svg',
                         ),
-                      ),
+                        Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.blue,
+                                width: 1.0,
+                              ),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(5.0))),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 16.0, vertical: 4.0),
+                            child: SvgPicture.asset(
+                              'assets/images/login/icons8-apple.svg',
+                              semanticsLabel: 'Apple svg',
+                              width: 25,
+                              height: 25,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.blue,
+                                width: 1.0,
+                              ),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(5.0))),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 16.0, vertical: 4.0),
+                            child: SvgPicture.asset(
+                              'assets/images/login/icons8-google.svg',
+                              width: 25,
+                              height: 25,
+                              semanticsLabel: 'Google svg',
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text("Don't have an account?"),
-                    TextButton(onPressed: () {}, child: const Text("Sign up"))
-                  ],
-                )
-              ]),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Don't have an account?"),
+                        TextButton(
+                            onPressed: () {}, child: const Text("Sign up"))
+                      ],
+                    )
+                  ]),
+            ),
+          ],
         ),
       ),
     ),
