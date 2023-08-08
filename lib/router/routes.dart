50edae760aeb6da6d61e7bfb1b5b7e4325cce651
EyasuTesfu <jt272525@gmail.com>
2023-08-08 05:18:23 +0300

    feat(joshua.feed): Add Go Router

---
+++ lib/router/routes.dart
@@ -0,0 +1,7 @@
+class AppRoutes {
+  static const SignupPage = '/signup-page';
+  static const LoginPage = '/login-page';
+  static const Feed = '/';
+  static const SplashPage = '/splash-page';
+  static const CreatePostPage = '/createpost-page';
+}
