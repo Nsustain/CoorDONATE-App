ba0c41b87e6e6dce2ac8e43561b84a1a8f245660
bereket <bereketdemissie4382@gmail.com>
2023-09-05 11:25:17 +0300

    feat(chat) delete and update chat usecases

--- lib/features/chat/domain/repositories/chat_message_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:coordonate_app/core/core.dart';
-import 'package:coordonate_app/features/chat/domain/entities/chat_entity.dart';
-import 'package:coordonate_app/features/chat/domain/usecases/update_text.dart';
-import 'package:dartz/dartz.dart';
-
-abstract class ChatRepository {
-  Future<Either<Failure, Chat>> updateMsg(id, message);
-  Future<Either<Failure, Chat>> deleteMsg(id);
-}
