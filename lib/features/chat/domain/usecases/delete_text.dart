044f803acd782d8a65d27ddc2d8f0edc8d3ce507
bereket <bereketdemissie4382@gmail.com>
2023-09-05 11:18:50 +0300

    feat(chat) delete and update message usecases

---
+++ lib/features/chat/domain/usecases/delete_text.dart
@@ -0,0 +1,11 @@
+import 'package:coordonate_app/core/core.dart';
+import 'package:coordonate_app/features/chat/domain/entities/chat_entity.dart';
+import 'package:coordonate_app/features/chat/domain/repositories/chat_message_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class DeleteText extends UseCase<Chat, Chat> {
+  final ChatRepository _repo;
+  DeleteText(this._repo);
+  @override
+  Future<Either<Failure, Chat>> call(Chat params) => _repo.deleteMsg(params.id);
+}
